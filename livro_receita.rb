5f69dd54b26d01359949377c3a9db1ad09075049
Murilo Ramos <muri.ilo@hotmail.com>
2021-02-23 17:10:30 -0300

    inclusão botão buscar por nome de receita

--- livro_receita.rb
+++ livro_receita.rb
@@ -1,8 +1,14 @@
+INSERIR = 1
+VISUALIZAR_RECEITAS = 2
+PESQUISAR_RECEITA = 3
+SAIR = 0
+
 puts 'Bem vindo ao livro de receitas'
 def menu()
-  puts "[1] Cadastrar uma receita"
-  puts "[2] Ver todas as receitas"
-  puts "[3] Sair"
+  puts "[#{INSERIR}] Cadastrar uma receita"
+  puts "[#{VISUALIZAR_RECEITAS}] Ver todas as receitas"
+  puts "[#{PESQUISAR_RECEITA} Pesquisar por uma receita"
+  puts "[#{SAIR}] Sair"
   print "Escolha uma opção: "
   return gets.to_i()
 end
@@ -13,12 +19,9 @@ def inserir_receita()
   nome_receita = gets.chomp()
   puts 'Digite o tipo: '
   tipo = gets.chomp()
-
-  
-
   puts "Receita #{nome_receita} cadastrada com sucesso!\n\n"
-
   return {nome_receita: nome_receita, tipo: tipo}
+
 end
 
 def imprimir_receitas(receitas)
@@ -28,29 +31,49 @@ def imprimir_receitas(receitas)
   receitas.each do |receita|
     puts "\n #{receita[:nome_receita]} -- #{receita[:tipo]} \n"
   end
+  if receitas.empty?
+    puts "nenhuma receita cadastrada"
+  end
 end
 
+def buscar_receitas(receitas)
+  puts "Digite o nome da receita"
+  receita = gets.chomp()
+
+  receitas.each do |nome|
+    nome.include? (receita)
+      puts
+      puts "#{nome[:nome_receita]}"
+      puts
+  end
+end
 
 receitas = []
 
 opcao = menu()
-while opcao != 3 do
-  if opcao == 1
+loop do
+  if opcao == INSERIR
 
     receitas << inserir_receita()
      
-  elsif opcao == 2
+  elsif opcao == VISUALIZAR_RECEITAS
 
     imprimir_receitas(receitas)
 
     # o ruby entende que um puts (nome_array) significa que 
     #o usuario quer mostrar os dados do array
+  elsif opcao == SAIR
+
+    break
+
+  elsif opcao == PESQUISAR_RECEITA
+    buscar_receitas(receitas)
+
   else
 
-  puts "Opção inválida"
+    puts "Opção inválida"
   
   end
 
   opcao = menu()
-
 end
