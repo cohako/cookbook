962f415846867725436f852d64f62727b581394f
Murilo Ramos <muri.ilo@hotmail.com>
2021-02-23 10:26:08 -0300

    Utilização de if e while

--- livro_receita.rb
+++ livro_receita.rb
@@ -1,1 +1,38 @@
-a
+puts 'Bem vindo ao livro de receitas'
+
+receitas = []
+
+puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+
+print "Escolha uma opção: "
+opcao = gets.to_i()
+
+while opcao != 3 do
+  if opcao == 1
+    puts 'Digite o nome da receita: '
+    nome_receita = gets.chomp()
+    receitas << nome_receita
+    puts "Receita #{nome_receita} cadastrada com sucesso!\n"
+    puts 'Adicionar nova receita?'
+  
+  elsif opcao == 2
+    puts '=======Receitas cadastradas======='
+    #o each nada mais faz que percorrer o array e jogar dentro
+    #de uma variavel de bloco
+    receitas.each do |receita|
+      puts receita
+    end
+    # o ruby entende que um puts (nome_array) significa que 
+    #o usuario quer mostrar os dados do array
+  else
+  puts "Opção inválida"
+  end
+  puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+
+print "Escolha uma opção: "
+opcao = gets.to_i()
+end
